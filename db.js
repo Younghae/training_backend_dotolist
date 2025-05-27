6c08d9ac5f837f9c84c3094ce67fa148e42539d2
jonathan <jonathan@globallinkers.co.kr>
2025-05-27 14:13:47 +0900

    변경

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'wlrwkdtkeh1',
+  password: 'qwer1234',
   database: 'todo_db'
 });
 module.exports = pool.promise();
